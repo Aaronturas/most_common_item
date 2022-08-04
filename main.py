588a52332ba421b9926ad25901bc163fc03edd1c
Aaronturas <91685385+Aaronturas@users.noreply.github.com>
2022-08-04 02:23:09 +0000

    Added more comments for understanding and reduce the size of variable names for clear code

--- main.py
+++ main.py
@@ -16,22 +16,44 @@ def most_common_item(my_list):
             mcc = count
     return mci
 
-# Test Cases list
+def most_common_item_dict(my_list):
+  item_counts = {}
+  for item in my_list:
+      if item in item_counts: #if item is not in dictionary then it is add
+          item_counts[item] += 1
+      else:
+          item_counts[item] = 1
+        
+  mci = None #most common item
+  mcc = None #most common count
+  for item in my_list:
+    count = 0
+    for item2 in my_list:
+      if item2 == item:
+        count += 1
+        if mcc is None or count > mcc:
+          mci = item
+          mcc = count
+  return mci
+
+# Test Cases
 my_list = [1, 2, 3, 3, 4, 2, 2, 1, 3, 2]
 start_time = time.time()
-result = most_common_item(my_list)
+list_result = most_common_item(my_list)
 end_time = time.time()
 total_time = end_time - start_time
+print("List Implementation")
 print("Input:", my_list)
-print("Output:", most_common_item(my_list))
+print("Output:", list_result)
 print("Time", total_time)
 
-# Test Cases dictonary
-my_list = {1, 2, 3, 3, 4, 2, 2, 1, 3, 2}
+
 start_time = time.time()
-result = most_common_item(my_list)
+dict_result = most_common_item_dict(my_list)
 end_time = time.time()
 total_time = end_time - start_time
-print("\nInput:", my_list)
-print("Output:", most_common_item(my_list))
-print("Time", total_time)
+
+print("\nDictionary Implementation")
+print("Input:", my_list)
+print("Output:", dict_result)
+print("Time", total_time)#Dictionary method is faster
